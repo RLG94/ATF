7b658c7fd405435174d2eebb3e1f2b7472aeea60
andonialvarez <andonialvarez3@gmail.com>
2024-05-10 20:22:32 +0200

    Navbar almost finished and Routing complete

--- APPTF/src/app/app.component.ts
+++ APPTF/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ReglamentoComponent } from './components/reglamento/reglamento.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, RankingComponent, CalendarioComponent, ErrorComponent, ReglamentoComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- APPTF/src/app/components/navbar/navbar.component.ts
+++ APPTF/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RankingComponent } from '../ranking/ranking.component';
+import { HomeComponent } from '../home/home.component';
+import { ErrorComponent } from '../error/error.component';
+import { CalendarioComponent } from '../calendario/calendario.component';
+import { ReglamentoComponent } from '../reglamento/reglamento.component';
+import { RouterModule,RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RankingComponent,HomeComponent,ErrorComponent,ReglamentoComponent,CalendarioComponent,RouterModule,RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
