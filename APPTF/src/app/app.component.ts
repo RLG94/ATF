3394c47ec80914e7f0cf0d39f61313c8fe4e875a
Roberto <roberto.lago.ubeda@gmail.com>
2024-05-11 14:03:40 +0200

    folder docs qith page

--- APPTF/src/app/app.component.ts
+++ APPTF/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { RankingComponent } from './components/ranking/ranking.component';
 import { CalendarioComponent } from './components/calendario/calendario.component';
 import { ErrorComponent } from './components/error/error.component';
 import { ReglamentoComponent } from './components/reglamento/reglamento.component';
 import { HomeComponent } from './components/home/home.component';
+
 
 @Component({
   selector: 'app-root',
