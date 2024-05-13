b346a880410d3f054f20f49893e669fb789ffaac
andonialvarez <andonialvarez3@gmail.com>
2024-05-13 11:26:00 +0200

    Tabla de ranking, v.1

--- APPTF/src/app/app.config.ts
+++ APPTF/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter,withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withHashLocation()), provideClientHydration()]
+  providers: [provideRouter(routes,withHashLocation()), provideClientHydration(), provideAnimationsAsync()]
 };
--- APPTF/src/app/components/ranking/ranking.component.ts
+++ APPTF/src/app/components/ranking/ranking.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule aquí
+
+interface usuario {
+  id: string;
+  nombre: string;
+  puntis: number;
+  ganador: number;
+  posicion?: number | null;  // La propiedad es opcional, indicada por el signo '?'
+}
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ranking.component.html',
-  styleUrl: './ranking.component.scss'
+  styleUrls: ['./ranking.component.scss'] 
 })
-export class RankingComponent {
+export class RankingComponent implements OnInit {
+  usuarios: usuario[] = [  // Usar la interfaz Usuario para asegurar la tipificación
+  { id: '1', nombre: 'Roberto Lago', puntis: 10, ganador: 2, posicion: null },
+  { id: '2', nombre: 'Andoni Alvarez', puntis: 7, ganador: 1, posicion: null },
+  { id: '3', nombre: 'Adrian Martinez', puntis: 12, ganador: 0, posicion: null },
+  { id: '4', nombre: 'Rebeca Llacer', puntis: 42, ganador: 0, posicion: null },
+  { id: '5', nombre: 'Maria Cabrera', puntis: 2, ganador: 0, posicion: null }
+  ];
+  generarArray(num: number): any[] {
+    return new Array(num);
+  }
 
-}
+  ngOnInit() {
+    this.ordenarPorPuntis();
+  }
+  
+  constructor() {
+    this.ordenarPorPuntis();
+    this.ordenarPorNombre();
+
+  }
+
+  ordenarPorPuntis() {
+    this.usuarios.sort((a, b) => b.puntis - a.puntis);
+    this.usuarios.forEach((usuario, index) => {
+      usuario.posicion = index + 1;
+    });
+  }
+  ordenarPorNombre() {
+    this.usuarios.sort((a, b) => {
+      return a.nombre.localeCompare(b.nombre, 'es', { sensitivity: 'base' });
+    });
+  }
+  getStyle(posicion: number) {
+    switch(posicion) {
+      case 1:
+        return { 'background-color': 'goldenrod' };
+      case 2:
+        return { 'background-color': 'silver' };
+      case 3:
+        return { 'background-color': '#762c28' };
+      default:
+        return {};
+    }
+  }
+  textoBusqueda: string = '';
+  usuarioEncontrado: any = null;
+
+  buscarUsuario() {
+    this.usuarioEncontrado = this.usuarios.find(usuario =>
+      usuario.nombre.toLowerCase().includes(this.textoBusqueda.toLowerCase())
+    );
+  
+    if (this.usuarioEncontrado) {
+      console.log(this.usuarioEncontrado.nombre);
+    } else {
+      console.log('No se encontró ningún usuario');
+    }
+  }
+  }
+
+
+
+
+
